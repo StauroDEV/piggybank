8a88ec9cfed321ba84f6b6844cfa309f80f6aaf6
v1rtl <hi@v1rtl.site>
2023-11-07 14:43:49 +0200

    remove default params

--- tests/globalSetup.ts
+++ tests/globalSetup.ts
@@ -1,10 +1,8 @@
 import { FORK_BLOCK_NUMBER, FORK_URL } from './constants.js'
 import { startProxy } from '@viem/anvil'
 
-export default async function () {
+export default async function() {
   return await startProxy({
-    port: 8545, // By default, the proxy will listen on port 8545.
-    host: '::', // By default, the proxy will listen on all interfaces.
     options: {
       chainId: 5,
       forkUrl: FORK_URL,
