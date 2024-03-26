3dc5a989a3c70baaca78e7d63587b5e2eeffaa78
jamesmccomish <james.mcc94@hotmail.co.uk>
2024-03-26 22:43:23 +0100

    fix: owner check

--- src/actions/public/getSafeOwners.test.ts
+++ src/actions/public/getSafeOwners.test.ts
@@ -3,11 +3,12 @@ import { getSafeOwners } from './getSafeOwners.js'
 import { publicClient } from '../../../tests/utils.js'
 import { EIP3770Address } from '../../types.js'
 import { EXAMPLE_SAFE, TEST_ADDRESS } from '../../../tests/constants.js'
+import { parseEip3770Address } from '../../utils/eip-3770.js'
 
 describe('getSafeOwners', () => {
   it('should retrieve the safes owners', () => {
     getSafeOwners(publicClient, EXAMPLE_SAFE as EIP3770Address).then((owners) => {
-      expect(owners).includes(TEST_ADDRESS)
+      expect(owners).includes(parseEip3770Address(TEST_ADDRESS).address)
     })
   })
 })
