f4e8ab8b442a2a03081dfe9870304617b5d42c87
v1rtl <hi@v1rtl.site>
2025-01-30 01:54:50 +0200

    use prool instead of deprecated @viem/anvil

--- tests/globalSetup.ts
+++ tests/globalSetup.ts
@@ -1,13 +1,14 @@
 import { sepolia } from 'viem/chains'
 import { FORK_BLOCK_NUMBER, FORK_URL } from './constants.js'
-import { startProxy } from '@viem/anvil'
+import { createServer } from 'prool'
+import { anvil } from 'prool/instances'
 
 export default async function () {
-  return await startProxy({
-    options: {
+  return createServer({
+    instance: anvil({
       chainId: sepolia.id,
       forkUrl: FORK_URL,
       forkBlockNumber: FORK_BLOCK_NUMBER,
-    },
+    }),
   })
 }
--- tests/setup.ts
+++ tests/setup.ts
@@ -1,7 +1,23 @@
 import { FORK_BLOCK_NUMBER, FORK_URL } from './constants.js'
 import { pool, testClient } from './utils.js'
-import { fetchLogs } from '@viem/anvil'
 import { afterAll, afterEach } from 'vitest'
+
+async function fetchLogs(url: string, id: number): Promise<string[]> {
+  const response = await fetch(new URL(`${id}/logs`, url), {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+  const result = (await response.json())
+
+  if (result.success === false) {
+    throw new Error(result.reason)
+  }
+
+  return result.logs
+}
 
 afterAll(async () => {
   // If you are using a fork, you can reset your anvil instance to the initial fork block.
