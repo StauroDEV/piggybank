bd5088689fc8a0237a10eb813ff9effa4c555f59
v1rtl <hi@v1rtl.site>
2025-01-30 02:06:03 +0200

    try this

--- tests/globalSetup.ts
+++ tests/globalSetup.ts
@@ -1,15 +1,12 @@
 import { sepolia } from 'viem/chains'
 import { FORK_BLOCK_NUMBER, FORK_URL } from './constants.js'
-import { createServer } from 'prool'
 import { anvil } from 'prool/instances'
 
 export default async function () {
-  const server = createServer({
-    instance: anvil({
-      chainId: sepolia.id,
-      forkUrl: FORK_URL,
-      forkBlockNumber: FORK_BLOCK_NUMBER,
-    }),
+  const server = anvil({
+    chainId: sepolia.id,
+    forkUrl: FORK_URL,
+    forkBlockNumber: FORK_BLOCK_NUMBER,
   })
 
   return server.start()
